be55038d6c9aa59357095c424264d186f2880501
a123verkho <alexander.verkholomov@gmail.com>
2021-04-11 22:06:39 +0300

    3

---
+++ homework_4/Prod_1_K_means_medoids.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 11 21:56:22 2021
+
+@author: AVERKHO
+"""
+
+import numpy as np
+from sklearn_extra.cluster import KMedoids
+
+X=np.array([[0,4,0,-5],[-6,4,0,2]])
